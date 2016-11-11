d3fccdd620fe8af28b4760de630c36557a753aa8
world <yyll9933@gmail.com>
2016-11-11 18:12:11 +0900

    muliple commnad read and file read write check

--- Main.java
+++ Main.java
@@ -3,17 +3,20 @@ public class Main{
 	public static void main(String[] args){
 
 		CommandCheck check = new CommandCheck();
-		Format format;
-		MDElement element;
-
+		Format format = new Format();
+		
 		format = check.receive(args);
 
-		if(format == null) return;
+		if(format.getnumOfFile() == 0) return;
 
-		DocumentParser dParser = new DocumentParser();
-		NodeParser nParser = new NodeParser();
-		Transform transfrom = new Transform();
-
+		for(int i = 0;i<format.getnumOfFile();i++){
+			DocumentParser dParser = new DocumentParser();
+			dParser.readLine(format,i);
+			NodeParser nParser = new NodeParser();
+			Transform transfrom = new Transform();
+			transfrom.createFile(format,i);
+		}
+			
 		return;
 	}
 		
--- transform.java
+++ transform.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 public class Transform{
 
 	public void Transform(){
@@ -6,5 +8,43 @@ public class Transform{
 		
 	}
 		
+	public void createFile(Format format, int index){
+		String txt = "Test file" ;
+         
+        String fileName = format.getOutputIndex(index);
+         
+         int i=1;
+         
+        try{
+             
+            File file = new File(fileName) ;
+
+            while(true){
+            	if (file.exists()){
+     
+      				fileName = fileName.substring(0,format.getOutputIndex(index).length()-5) + "-" + i + ".html";
+      				file = new File(fileName);
+     				
+     				i++;
+     				
+     			}
+     			else
+     				break;
+             }
+   
+            FileWriter fw = new FileWriter(file, false) ;
+             
+            fw.write(txt);
+            fw.flush();
+ 
+            fw.close(); 
+             
+             
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+	}
 	
 }

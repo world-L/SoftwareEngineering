45afed08effba867dd622caae2348729be069c5a
yelyn <yelyn1249@gmail.com>
2016-11-25 19:40:44 +0900

    token class 파일추가

---
+++ project/src/HTMLCode.java
@@ -0,0 +1,20 @@
+public class HTMLCode extends Tokens{
+
+	private String text;
+
+	public HTMLCode(String text){
+		this.text = text;
+	}
+
+	public void accept(MDElementVisitor visitor){
+
+	}
+	public void htmlTransform()
+	{
+		//html code
+
+
+	}
+
+
+}
